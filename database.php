bfb3b2b0ff55cc9c5cd8c6efa66dc092ae2072c2
DrDisagree <crazymahmud08@gmail.com>
2023-11-15 12:49:53 +0600

    Format codes

--- database.php
+++ database.php
@@ -1,12 +1,12 @@
 <?php
-    $hostname = "localhost";
-    $dbusername = "root";
-    $dbpassword = "";
-    $dbname = "user_database";
+$hostname = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "user_database";
 
-    $db_connection = mysqli_connect($hostname, $dbusername, $dbpassword, $dbname);
+$db_connection = mysqli_connect($hostname, $dbusername, $dbpassword, $dbname);
 
-    if (!$db_connection) {
-        die("Something went wrong...");
-    }
+if (!$db_connection) {
+    die("Something went wrong...");
+}
 ?>
